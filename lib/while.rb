f4a3a108501461f96d0f37a9193baf55056bb232
Chris Beyer <beyercorpuz@gmail.com>
2020-01-25 04:33:12 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,5 +1,10 @@
 def using_while
   levitation_force = 6
+  counter = 0
+while counter < 10
+  puts "Whie your levitation force is less than 10"The current number is less than 10."
+  counter += 1
+end
 	
   #your code here
 end
